41d3baf696ce6d45248aee46746d6c0b2abfb547
Gairat13 <kairat9696kg@mail.ru>
2021-03-01 15:02:15 +0600

    Some commit

---
+++ account/utils.py
@@ -0,0 +1,13 @@
+from django.core.mail import send_mail
+
+
+def send_activation_mail(user):
+    user.create_activation_code()
+    message = f"""Thank you for registration. Please tap the link to activate your account:
+    http://127.0.0.1:8000/accounts/activation/?u={user.activation_code}"""
+    send_mail(
+        'Активация аккаунта',
+        message,
+        'test@my_project.com',
+        [user.email],
+    )
